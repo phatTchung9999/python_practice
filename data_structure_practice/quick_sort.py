bb0daee2b5fc3a50d1f8a72774e77176146da06f
phatTchung9999 <chyngtanphat@gmail.com>
2025-12-22 23:20:14 -0500

    Update quick_sort.py

--- data_structure_practice/quick_sort.py
+++ data_structure_practice/quick_sort.py
@@ -4,11 +4,16 @@ def quickSort(arr):
     else:
         pivot = arr[0]
 
-        smaller = [i for i in arr if i <=  pivot]
+        smaller = [i for i in arr[1:] if i <=  pivot]
 
-        greater = [i for i in arr if i > pivot]
+        greater = [i for i in arr[1:] if i > pivot]
 
-        return quickSort(smaller) + pivot + quickSort(greater)
+        return quickSort(smaller) + [pivot] + quickSort(greater)
     
 
-[56,25,68,51,25,48]
+def main():
+    print(quickSort([100,45,90,55,23,1,67]))
+
+
+if __name__ == '__main__':
+    main()
